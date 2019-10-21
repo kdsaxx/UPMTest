e692c788f44731753f14338dd344643d5fd4922a
ParkJinHo <vvx38@naver.com>
2019-10-21 16:41:45 +0900

    Unity Editor

---
+++ Editor/Window.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Window : EditorWindow
+{
+    [MenuItem("Window/Example")]
+    static void Open()
+    {
+        var exampleWindow = CreateInstance<Window>();
+        exampleWindow.Show();
+    }
+
+}
